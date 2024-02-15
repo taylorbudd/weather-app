b20d4a00b3e3dc64124c4ce1cdd7b1c7a4fa2dfc
Taylor Budd <taylorbudd4@gmail.com>
2024-02-15 13:28:45 -0400

    Remove api key and use env variable

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
+
+API_KEY=process.env.API_KEY
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -17,7 +20,7 @@ app.get('/search', async (req, res) => {
     console.log("hey")
     cityName = req.query.city
     console.log("city name: " + cityName)
-    let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=aa0e088e4a4b39d7e3dcf8f1e4af8cf0&units=metric`
+    let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`
     console.log("API URL: " + API_URL);
     const response = await fetch(API_URL);
     const data = await response.json()
